7c4f35c87b35476013b685224ea879ee6c2aad35
bsellier <b.sellier27@hotmail.fr>
2026-05-11 16:32:43 +0200

    Added topic_ltree column

--- src/initialize_fill_tables.sh
+++ src/initialize_fill_tables.sh
@@ -3,7 +3,7 @@ if [[ $FILL_TABLES_INITIALIZE == false || $FILL_TABLES_INITIALIZE == 0 ]]; then
     exit
 fi
 
-INIT_FILL_DAY=$(date -d "3 days ago" +%Y-%m-%d)
+INIT_FILL_DAY=$(date -I -d "3 days ago")
 # INIT_FILL_DAY=$(date -d "@$(($(date +%s) - 259200))" +%Y-%m-%d)
 /datanommer-ctl/curl_day_data.sh $INIT_FILL_DAY
 /datanommer-ctl/ingest_day.sh $INIT_FILL_DAY
